d0b7e5fa396a488d16dabf3f163d1493af85c4d4
Alessandro Brito <alessandrosouzabrito@gmail.com>
2026-06-03 12:23:31 -0300

    Adicionado arquivo de Treino 6

---
+++ Treino6.js
@@ -0,0 +1,12 @@
+var A = 10;
+var B = 20;
+
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(B / A);
+console.log(B % A);
+console.log(A**2);
+console.log(++A);
+console.log(--A);
+
