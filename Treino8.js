643ca2f253fa256922dde31560e420e703f16f09
Alessandro Brito <alessandrosouzabrito@gmail.com>
2026-06-05 12:30:38 -0300

    Adicionado arquivo de Treino 9

--- Treino8.js
+++ Treino8.js
@@ -6,22 +6,22 @@ console.log(C); //atribui o valor de A + B na variável C = 50
 
 var D = 10;
 D += A;
-console.log(D); //equivale a D = D + A = 40
+console.log(D); //Equivale a D = D + A = 40
 
 var E = 40;
 E -= A;
-console.log(E); // equivale a E = E - A = 10
+console.log(E); //Equivale a E = E - A = 10
 
 var F = 2;
 F *= A;
-console.log(F); //equivale a F = F * A = 60
+console.log(F); //Equivale a F = F * A = 60
 
 var G = 300;
 G /= A;
-console.log(G); // equivale a G = G / A = 10
+console.log(G); //Equivale a G = G / A = 10
 
 var H = 130;
 H %= B;
-console.log(H); //equivale a H = H % B = 10
+console.log(H); //Equivale a H = H % B = 10
 
 
