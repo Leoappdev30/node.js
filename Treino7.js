4994ef52a25e5959e7d629b96fd1ba20719d8247
Alessandro Brito <alessandrosouzabrito@gmail.com>
2026-06-05 10:23:10 -0300

    Adicionado arquivo de Treino 7

---
+++ Treino7.js
@@ -0,0 +1,11 @@
+var A = 10;
+var B = 20;
+
+console.log(A == B);
+console.log(A === B);
+console.log(A != B);
+console.log(A > B);
+console.log(A < B);
+console.log(A >= B);
+console.log(A <= B);
+
