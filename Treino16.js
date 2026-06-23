0c870a338a0a4cc96f59082cfa43c003f377f3f3
unknown <202220765@uesb.edu.br>
2026-06-23 11:27:17 -0300

    Adiciona Treino16.js

---
+++ Treino16.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+
+var numero = prompt("Digite um número: ");
+
+if (numero % 2 == 0)
+{
+    console.log("O número é par");
+}
+else
+{
+    console.log("O número é ímpar");
+}
