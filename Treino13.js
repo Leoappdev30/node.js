07dbd9d2f65852ad72f8ca8263d5bdb49651a883
unknown <202220765@uesb.edu.br>
2026-06-23 08:49:56 -0300

    Publicado commit Treino13

---
+++ Treino13.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+
+var x = Number(prompt("Digite um número: "));
+console.log("Mostre o número:" + x );
+
+var y = Number(prompt("Digite outro número: "));
+console.log("Mostre o número: " + y);
+
+var soma = x + y;
+console.log("A soma é: " + soma);
+
+
+
