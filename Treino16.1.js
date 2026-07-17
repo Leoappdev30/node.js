8ea695bb1019b6a12739b1b602bc115f3b99a9f7
Alessandro Brito <alessandrosouzabrito@gmail.com>
2026-07-17 10:22:13 -0300

    Adicionado arquivo de Treino 16.1

---
+++ Treino16.1.js
@@ -0,0 +1,17 @@
+var A = 120;
+var B = 200;
+
+if (A > B)
+{
+    console.log("A é maior que B");
+}
+else if (A < B)
+{
+    console.log("B é maior que A");
+}
+else
+{
+    console.log("A é B são iguais")
+}
+
+    
