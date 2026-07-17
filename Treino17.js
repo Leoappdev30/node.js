cae7842efca2fdce87833fb9be7f08928695606c
Alessandro Brito <alessandrosouzabrito@gmail.com>
2026-07-17 10:37:05 -0300

    Adicionado arquivo de Treino 17 Treino 17: Adicionado arquivo de Treino 17

---
+++ Treino17.js
@@ -0,0 +1,12 @@
+var i = -10;
+
+if (i > 0)
+{
+    if (i <= 100)
+{
+    console.log("E um número positivo menor que 100");
+}
+else{
+    console.log("i é um numero positivo maior que 100");
+     }
+}
