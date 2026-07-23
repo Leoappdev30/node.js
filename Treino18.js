a9d16a8792657c46b659754740780482b52ae961
Alessandro Brito <alessandrosouzabrito@gmail.com>
2026-07-23 12:39:07 -0300

    Adicionado Treino 18

---
+++ Treino18.js
@@ -0,0 +1,17 @@
+var x = 15;
+
+switch(x)
+{
+    case 5:
+        console.log("valor de x é 5");
+        break;
+    case 10:
+        console.log("valor de x é 10");
+        break;
+    case 15:
+        console.log("valor de x é 15");
+        break;
+    default:
+        console.log("valor desconhecido");    
+
+}
